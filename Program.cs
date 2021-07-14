0fd25514108553887d2e6b45d76b34462adb9da6
Nikita Lysenko <nikita.lysenko11@gmail.com>
2021-07-14 20:19:50 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace test
+{
+    class MyClass
+    {
+        public string a, b, c;
+
+        public MyClass(string a, string b, string c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+    }
+          
+    class Program
+    {
+        static void Main()
+        {
+            MyClass[] Array = new MyClass[10];
+StartPoint:
+            Console.WriteLine("A :");
+            string a = Console.ReadLine();
+            Console.WriteLine("B :");
+            string b = Console.ReadLine();
+            Console.WriteLine("C :");
+            string c = Console.ReadLine();
+                       
+            Console.WriteLine("Save?");
+            string save = Console.ReadLine();
+            int arraycount = 0;
+            
+            if (save == "yes")
+            {
+                Array[arraycount] = new MyClass(a, b, c);
+                arraycount++;
+            }
+
+            Console.WriteLine("More ABC?");
+            string more = Console.ReadLine();
+
+            if (more == "yes")
+            {
+                goto StartPoint;
+            }
+
+            for (int i = 0; i <= arraycount; i++)
+            {
+                Console.WriteLine(Array[i].a);
+                Console.WriteLine(Array[i].b);
+                Console.WriteLine(Array[i].c);
+                Console.WriteLine(new string('-', 20));
+            }
+
+            Console.ReadKey();
+          
+
+        }
+    }
+}
